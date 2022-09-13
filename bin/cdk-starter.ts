a59377b43be8fb203b203cb0032b7ab25c56fbd4
Hasan <dev.hahuaz@gmail.com>
2022-09-13 12:03:50 +0300

    add pipeline

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AppStack } from '../lib/app-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new AppStack(app, 'MyAppStack', {
+
+const branch = app.node.tryGetContext('branch');
+const { account, region, appName } = app.node.tryGetContext(branch);
+
+new PipelineStack(app, `${branch}-${appName}-pipelineStack`, {
   env: {
-    region: process.env.CDK_DEFAULT_REGION,
-    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region,
+    account,
   },
 });
+
+app.synth();
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,11 +1,15 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as path from 'path';
+import { Construct } from 'constructs';
 
 export class AppStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const branch = this.node.tryGetContext('branch');
+    const { region } = this.node.tryGetContext(branch);
 
     const myFunction = new NodejsFunction(this, 'my-function', {
       memorySize: 1024,
@@ -15,6 +19,9 @@ export class AppStack extends cdk.Stack {
       entry: path.join(__dirname, `/../src/lambdas/example/index.ts`),
       bundling: {
         minify: true,
+      },
+      environment: {
+        APP_REGION: region,
       },
     });
   }
